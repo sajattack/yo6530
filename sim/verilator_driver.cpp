a5f11702e1014e40f2c1835c2834b5eb39ad8fee
Paul Sajna <hello@paulsajna.com>
2023-12-19 22:09:16 -0800

    test skeleton

--- sim/verilator_driver.cpp
+++ sim/verilator_driver.cpp
@@ -11,6 +11,31 @@ double sc_time_stamp() {
     return (double) tickcount;
 }
 
+void check_rom(Vverilator_top* top, VerilatedVcdC* trace) {
+    for (int i=0; i<10000; i++) {
+        top->PHI2 = !(top->PHI2);
+        if (i%10==0)
+        {
+            top->addr = top->addr+1;
+        }
+        top->eval();
+        trace->dump(10*tickcount);
+        tickcount++;
+    }
+}
+
+void check_ram(Vverilator_top* top, VerilatedVcdC* trace) {
+    
+}
+
+void check_io(Vverilator_top* top, VerilatedVcdC* trace) {
+    
+}
+
+void check_timer(Vverilator_top* top, VerilatedVcdC* trace) {
+    
+}
+
 int main(int argc, char** argv) {
     Verilated::commandArgs(argc, argv);
     Vverilator_top *top = new Vverilator_top;
@@ -47,19 +72,15 @@ int main(int argc, char** argv) {
     trace->dump(10*tickcount);
     tickcount++;
 
-    for (int i=0; i<10000; i++) {
-        top->PHI2 = !(top->PHI2);
-        if (i%10==0)
-        {
-            top->addr = top->addr+1;
-        }
-        top->eval();
-        trace->dump(10*tickcount);
-        tickcount++;
-    }
+    check_rom(top, trace);
+    check_ram(top, trace);
+    check_io(top, trace);
+    check_timer(top, trace);
 
     trace->close();
     delete trace;
     top->final();
     delete top;
 }
+
+
