c7030e04f2e8050301eea60cb4da7f40ab5ac960
Paul Sajna <hello@paulsajna.com>
2025-05-12 00:59:17 -0700

    more timer fixes

--- src/timer.v
+++ src/timer.v
@@ -12,8 +12,9 @@ module timer (
 );
 
   reg [9:0] timer_divider;
-  reg [9:0] timer_count;
-  reg [7:0] timer;
+  reg [9:0] timer;
+  reg [7:0] timer_count_reg_in;
+  reg [7:0] timer_count_reg;
 
   reg [7:0] reg_data;
   reg [2:0] reg_addr;
@@ -34,8 +35,8 @@ module timer (
   always @(negedge clk) begin
     if (~rst_n) begin
         timer_divider <= 10'd0;
-        timer_count <= 10'd0;
-        timer <= 8'd0;
+        timer <= 10'd0;
+        timer_count_reg_in <= 8'd0;
         irq <= 1'b0;
         irq_en <= 1'b0;
     end else begin  
@@ -44,7 +45,8 @@ module timer (
             irq_en <= reg_addr[2]; 
             if (~we_n) begin
             // write timer counter
-                timer <= DI - 1;
+                timer_count_reg_in <= DI - 1 + 12;
+                timer_count_reg <= DI - 1 + 12;
                 // write divider based on address lines
                 case (reg_addr[1:0])
                     2'b00:   timer_divider <= 10'd0;
@@ -55,17 +57,22 @@ module timer (
             end
         end
 
-        if (timer == 8'd0) begin
+        if (timer_count_reg == 8'd0) begin
             irq <= ~(irq_en & 1'b1); // trigger irq if enabled when timer reaches 0
-            timer_count <= timer_count + 1;
+            timer <= timer + 1;
         end else begin
-        // increment the count
-            timer_count <= timer_count + 1;
+        // increment the timer
+            timer <= timer + 1;
         end
-        // decrement the timer when the divider is reached
-        if (timer_count == timer_divider) begin
-            timer <= timer - 1;
-            timer_count <= 0;
+        // decrement the timer_count (periods) when the divider is reached
+        if (timer == timer_divider) begin
+            timer_count_reg <= timer_count_reg - 1;
+            timer <= 0;
+        end
+        // reload timer_count_reg with timer_count_reg_in (start the next period)
+        // when timer_count_reg reaches 0
+        if (timer_count_reg == 0) begin
+            timer_count_reg <= timer_count_reg_in;
         end
     end
   end
